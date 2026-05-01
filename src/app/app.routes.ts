988340d659e54ec4b40401189fc775bf108e189a
Guirguis <ephraimhedia2@gmail.com>
2026-05-01 14:50:04 +0200

    Basic Setup For the Project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+import { NotfoundComponent } from './features/notfound/notfound.component';
+import { ProfileComponent } from './features/profile/profile.component';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { LoginComponent } from './features/login/login.component';
+import { RegisterComponent } from './features/register/register.component';
+import { FeedComponent } from './features/feed/feed.component';
+import { NotificationComponent } from './features/notification/notification.component';
+import { ForgotPasswordComponent } from './features/forgot-password/forgot-password.component';
+import { ChangePasswordComponent } from './features/change-password/change-password.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"",redirectTo:'login',pathMatch:'full'},
+    {path:"",component:AuthLayoutComponent,children:[
+        {path:"login",component:LoginComponent,title:"Login"},
+        {path:"register",component:RegisterComponent,title:"Register"},
+        {path:"forgot-password",component:ForgotPasswordComponent,title:"Forgot Password"},
+        {path:"change-password",component:ChangePasswordComponent,title:"Change Password"},
+    ]},
+    {path:"",component:MainLayoutComponent,children:[
+        {path:"profile",component:ProfileComponent,title:"Profile"},
+        {path:"feed",component:FeedComponent,title:"Feed"},
+        {path:"notification",component:NotificationComponent,title:"Notification"},
+    ]},
+    {path:"**",component:NotfoundComponent,title:"Not Found"},
+];
